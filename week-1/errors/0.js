f7e6bb6cd9859c91241d8005d91241fdfb1d9aaf
Priscilla Mac-Gatus <macgatuspriscilla@gmail.com>
2024-10-20 16:23:47 +0200

    Corrected the error

--- week-1/errors/0.js
+++ week-1/errors/0.js
@@ -1,10 +1,4 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
-
-
 //This showed as an error because it is suppose to be a comment
 
 //This is just an instruction for the first activity - but it is just for human consumption
 //We don't want the computer to run these 2 lines - how can we solve this problem?
-
-
