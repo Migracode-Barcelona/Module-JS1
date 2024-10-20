5656293d4d4d89fcc4a944f0c0bac09a7f7091de
Umair Riaz <umairriaz.ur2@gmail.com>
2024-10-20 07:06:18 +0200

    time-format completed

--- week-1/interpret/time-format.js
+++ week-1/interpret/time-format.js
@@ -27,3 +27,48 @@ console.log(result);
 //    Think of what values may cause problems for the code.
 //    Decide the result should be for those values, then test it out.
 //    Can you find any values of movieLength which don't give you what you'd expect?
+
+// ANSWER //
+/*
+---- a) Variable Declarations ----
+  There are 6 variable declarations in this program:
+    const movieLength = 8784;
+    const remainingSeconds
+    const totalMinutes
+    const remainingMinutes
+    const totalHours
+    const remainingHours
+    const result
+
+
+---- b) Number of Function Calls ----
+  There are 0 function calls in this program.
+
+
+---- c) Expression movieLength % 60 ----
+  It calculates the remainder when movieLength (in seconds) is divided by 60.
+
+  This operation determines the number of seconds that remain after converting movieLength into whole minutes. In this case, it tells you how many seconds are left.
+
+
+---- d) Interpretation of Line 4 ----
+  It calculates the remaining minutes after converting totalMinutes into whole hours.
+
+
+---- e) Interpretation of result ----
+`result` represents the formatted time of the movie in the format HH:MM:SS
+Better Name: movieTime
+
+
+---- f) Testing for Values of movieLength ----
+  Problems:
+    If movieLength is negative (e.g., -1000), it will produce negative hours and minutes, which doesn't make sense.
+    const movieLength = -1000; // Negative length
+
+    If movieLength is less than 60, it will still provide a valid output, but remainingHours and remainingMinutes will be 0, and only remainingSeconds will show the correct value. For example:
+    const movieLength = 30; // Less than a minute
+
+    If movieLength value is large (e.g., 1000000000), it will still calculate the result, but the hours could exceed unrealistically for movie lengths.
+    const movieLength = 1000000000; // Very large length
+
+*/
