7acb1bd5dc976a6cc3aa2ffa5662524d4a2998b1
Umair Riaz <umairriaz.ur2@gmail.com>
2024-10-21 23:37:15 +0200

    Implemented Angle Type

--- week-3/implement/get-angle-type.js
+++ week-3/implement/get-angle-type.js
@@ -21,3 +21,41 @@
 // Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
+
+function getAngleType(angleInput) {
+  if (angleInput === 90) {
+    return "Right angle";
+  } else if (angleInput < 90) {
+    return "Acute angle";
+  } else if (angleInput > 90 && angleInput < 180) {
+    return "Obtuse angle";
+  } else if (angleInput === 180) {
+    return "Straight angle";
+  } else if (angleInput > 180 && angleInput < 360) {
+    return "Reflex angle";
+  } else {
+    return "Wrong angle value!";
+  }
+}
+// Right Angle
+console.log(getAngleType(90));
+
+// Acute Angles
+console.log(getAngleType(45));
+console.log(getAngleType(89));
+
+// Obtuse Angles
+console.log(getAngleType(120));
+console.log(getAngleType(135));
+
+// Straight Angle
+console.log(getAngleType(180));
+
+// Reflex Angles
+console.log(getAngleType(200));
+console.log(getAngleType(270));
+
+// Invalid Angles
+console.log(getAngleType(-10));
+console.log(getAngleType(370));
+console.log(getAngleType(360)); // Expected: "Wrong angle value" (full circle)
