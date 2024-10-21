6c1fe52d6cd104d549084b4f4877248004232c02
Umair Riaz <umairriaz.ur2@gmail.com>
2024-10-21 19:00:33 +0200

    only contains week-3 work

--- week-1/errors/0.js
+++
@@ -1,8 +0,0 @@
-// ANSWER //
-/*
-By commenting it
-
-This is just an instruction for the first activity - but it is just for human consumption
- We don't want the computer to run these 2 lines - how can we solve this problem?
-
- */
