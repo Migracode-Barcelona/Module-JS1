d211d12d6196dbad90963fa63e19d349b94d7518
Said Akbarov <134784793+FrenchFry36@users.noreply.github.com>
2024-10-22 18:29:20 +0200

    exercises resolved

--- week-1/exercises/decimal.js
+++ week-1/exercises/decimal.js
@@ -1,6 +1,11 @@
-
 const num = 56.5467;
+const wholeNumberPart = Math.floor(num);
+const decimalPart = parseFloat((num - wholeNumberPart).toFixed(4));
+const roundedNum = Math.round(num);
 
+console.log(wholeNumberPart);
+console.log(decimalPart);
+console.log(roundedNum);
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 // Create a variable called wholeNumberPart and assign to it an expression that evaluates to 56 ( the whole number part of num )
