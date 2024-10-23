331ce40ff584ea28df17fe02873d0c9a497bc80f
¨Deborah <deborahgamedze7@gmail.com>
2024-10-23 16:29:44 +0200

    i describe  what line 3 is doing

--- week-1/exercises/count.js
+++ week-1/exercises/count.js
@@ -4,3 +4,5 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//line 3 adds 1 to count and updates to new value 1
