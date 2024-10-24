3539e44130b95279a6010ed6db4d5b39ff5670f2
Afere precious <preciousafere@gmail.com>
2024-10-24 16:14:14 +0200

    error 0.js solution

--- week-2/errors/0.js
+++ week-2/errors/0.js
@@ -2,8 +2,14 @@
 // write down the error you predict will be raised
 // then call the function capitalise with a string input
 // interpret the error message and figure out why it's happening, if your prediction was wrong
-
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+console.log("string input", `${capitalise("I am happy")}`);
+
+//Answer:
+// Identifier 'str' has already been declared
+//There's no need declaring it again.
+//When you fix the syntax error, a Reference error appears because it will try to access str outside the function
+//where it was not defined.
