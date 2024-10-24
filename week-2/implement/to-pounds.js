5e18658378d9e8753c0508e8232dea9af8d31496
Afere precious <preciousafere@gmail.com>
2024-10-24 16:57:52 +0200

    implement to-pounds.js solution

--- week-2/implement/to-pounds.js
+++ week-2/implement/to-pounds.js
@@ -3,3 +3,24 @@
 // Take this code and turn it into a reusable block of code.
 // Declare a function called toPounds with an appropriately named parameter.
 // Call this function a number of times to check it works for different inputs
+
+function toPounds(penceString) {
+  const penceStringWithoutTrailingP = penceString.substring(
+    0,
+    penceString.length - 1
+  );
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+  const pounds = paddedPenceNumberString.substring(
+    0,
+    paddedPenceNumberString.length - 2
+  );
+
+  const pence = paddedPenceNumberString
+    .substring(paddedPenceNumberString.length - 2)
+    .padEnd(2, "0");
+  return `£${pounds}.${pence}`;
+}
+console.log(toPounds("399p"));
+console.log(toPounds("1500p"));
+console.log(toPounds("2000p"));
+console.log(toPounds("2500p"));
