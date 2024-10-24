51b97c08c862d95978e20e863b05d0b1ffeb28ac
Afere precious <preciousafere@gmail.com>
2024-10-24 16:15:04 +0200

    error 1.js solution

--- week-2/errors/1.js
+++ week-2/errors/1.js
@@ -3,11 +3,20 @@
 // Why will an error occur when this program runs?
 // Play computer with the example to work out what is going on
 
+const decimalNumber = 0.5;
+
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log("The percentage of", `${convertToPercentage(decimalNumber)}`);
+console.log("The decimal number", `${decimalNumber}`);
+
+//Answer:
+// 1.syntaxError because decimalNumber has already been declared
+// 2.ReferenceError since decimalNumber was not defined outside the function
+
+//An error will will occur specifically on this line
+//console.log(decimalNumber); because decimalNumber was not defined outside the convertToPercentage() function
