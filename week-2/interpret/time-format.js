2a2b7446d64099b658a5e71e4d7351f23becc7e2
Afere precious <preciousafere@gmail.com>
2024-10-24 17:44:25 +0200

    interpret time-format.js

--- week-2/interpret/time-format.js
+++ week-2/interpret/time-format.js
@@ -26,18 +26,23 @@ console.log(formatTimeDisplay(143));
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
+//Answer:3 times
 
 // Call formatTimeDisplay with an input of 143, now answer the following:
-
 // b) What value is assigned to the parameter num when pad is called for the first time?
+//Answer: 00 since there is no full hours
 
 // c) What is the return value of pad when it is called for the first time?
+//Answer: The return value of the pad when its called for the first time is 00
 
 // d) What is the value assigned to the parameter num when pad
 // is called for the last time in this program?  Explain your answer
+//Answer : 23 , is the valued assigned to the parameter because its the value of the seconds.
 
 // e) What is the return value when pad is called
 // for the last time in this program?  Explain your answer
+//Answer : 23 , The last pad returns the 23 because its a 2-digit number so the function does not add 0 to it.
 
 // f) Research an alternative way of padding the numbers in this code.
 // Look up the string functions on mdn
+//Answer: instead of using the pad function we can use the String prototype padStart(), it is an in-build function
