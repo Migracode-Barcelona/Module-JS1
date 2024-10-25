71d80f826f59588dcbb834e978b14ccf1c36d6ba
Ebtesam <eebtesamm23@gmail.com>
2024-10-25 17:55:47 +0200

    my interpret

--- week-1/interpret/to-pounds.js
+++ week-1/interpret/to-pounds.js
@@ -27,3 +27,17 @@ console.log(`£${pounds}.${pence}`);
 // To begin, we can start with
 
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+/*  2 const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1);
+Removes the trailing "p" from the string. Result: "399". */
+
+ /* 3 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+Pads the string "399" to ensure it has at least 3 characters, but since it already has 3, it remains "399". */
+
+/*  4 const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
+Extracts the pound portion by taking all characters except the last two. Result: "3". */
+
+ /* 5 const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+Extracts the last two characters as pence. If there’s only one character, it pads to 2, but here it becomes "99". */
+
+/* 6 console.log(£${pounds}.${pence});
+Outputs the formatted price in pounds, resulting in "£3.99". */
