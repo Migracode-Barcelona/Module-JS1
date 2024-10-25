9460b816a192dd9f37f1089f3822326e1bf4447e
¨Deborah <deborahgamedze7@gmail.com>
2024-10-25 15:59:24 +0200

    used the function to round up

--- week-1/exercises/decimal.js
+++ week-1/exercises/decimal.js
@@ -8,10 +8,11 @@ const num = 56.5467;
 //console.log(wholeNumber)
 
 // Create a variable called decimalPart and assign to it an expression that evaluates to 0.5467 ( the decimal part of num )
-const decimalpart = num-wholeNumberPart;
+//const decimalpart = num-wholeNumberPart;
 
 
 
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
+const roundedNum =Math.round(num)
 
 // Log your variables to the console to check your answers
