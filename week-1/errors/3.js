8d216e1518f0cbba5617e495870674c4ce947513
Ebtesam <eebtesamm23@gmail.com>
2024-10-25 17:37:23 +0200

    errors fixed

--- week-1/errors/3.js
+++ week-1/errors/3.js
@@ -1,7 +1,13 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = String(cardNumber).slice(-4);
+console.log(last4Digits);
+
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
 // Make and explain a prediction about why the code won't work
+
+/* The slice() method is a string method, which means it can only be used on string values, not numbers. 
+To fix this convert cardNumber to a string */
+
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
