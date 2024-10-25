179f46125c04e81477e6cf873d15f9caef995d8f
Priscilla Mac-Gatus <macgatuspriscilla@gmail.com>
2024-10-25 14:03:26 +0200

    Predicted and explained and corrected debug

--- week-2/debug/2.js
+++ week-2/debug/2.js
@@ -12,3 +12,31 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+
+/*
+PREDICTION
+All the last digits of values in console.log will be 3.
+
+EXPLANATION
+
+Since the declare variable is not in the local scope but in the global scop and is not
+placed as a parameter in the function.
+Console.log will treat num as 103 always.
+
+
+CORRECTION
+
+const num = 103;
+
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`); // 2
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);// 5
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);// 6
+
+
+
+
+*/
