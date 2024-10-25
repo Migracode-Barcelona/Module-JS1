ed84c299622d84ac6eeda5ec51e73c8de04ce8e2
¨Deborah <deborahgamedze7@gmail.com>
2024-10-25 16:03:52 +0200

    i console.log to see solutions

--- week-1/exercises/decimal.js
+++ week-1/exercises/decimal.js
@@ -4,11 +4,11 @@ const num = 56.5467;
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 // Create a variable called wholeNumberPart and assign to it an expression that evaluates to 56 ( the whole number part of num )
-//const wholeNumberPart=Math.floor(num); 
+const wholeNumberPart=Math.floor(num); 
 //console.log(wholeNumber)
 
 // Create a variable called decimalPart and assign to it an expression that evaluates to 0.5467 ( the decimal part of num )
-//const decimalpart = num-wholeNumberPart;
+const decimalPart = num-wholeNumberPart;
 
 
 
@@ -16,3 +16,6 @@ const num = 56.5467;
 const roundedNum =Math.round(num)
 
 // Log your variables to the console to check your answers
+console.log(wholeNumberPart) //56
+console.log(decimalPart)
+console.log(roundedNum)
