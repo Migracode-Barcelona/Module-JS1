acad659f0639eafc2cb3875ca513f6c35c38af45
¨Deborah <deborahgamedze7@gmail.com>
2024-10-25 15:46:22 +0200

    i used math floor to remove decimal

--- week-1/exercises/decimal.js
+++ week-1/exercises/decimal.js
@@ -4,7 +4,14 @@ const num = 56.5467;
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 // Create a variable called wholeNumberPart and assign to it an expression that evaluates to 56 ( the whole number part of num )
+//const wholeNumber=Math.floor(num); 
+//console.log(wholeNumber)
+
 // Create a variable called decimalPart and assign to it an expression that evaluates to 0.5467 ( the decimal part of num )
+
+
+
+
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
 
 // Log your variables to the console to check your answers
