d2ff71b9e8450a3762b40764cfd35dda9d483d1e
¨Deborah <deborahgamedze7@gmail.com>
2024-10-25 16:41:15 +0200

    i explained what num represent

--- week-1/exercises/random.js
+++ week-1/exercises/random.js
@@ -4,6 +4,8 @@ const maximum = 100;
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
 // In this exercise, you will need to work out what num represents?
+//num represent any random number between 1-100  
+
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num several times to build an idea of what the program is doing
