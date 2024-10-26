8fac296c375712b54973021cc31a45789b81966b
¨Deborah <deborahgamedze7@gmail.com>
2024-10-26 04:41:17 +0200

    created a variable

--- week-1/exercises/paths.js
+++ week-1/exercises/paths.js
@@ -15,7 +15,7 @@ const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
-
+const dirpart =filePath.substring( 0,filePath.lastIndexOf(¨/¨));
 
 
 // Create a variable to store the ext part of the variable
