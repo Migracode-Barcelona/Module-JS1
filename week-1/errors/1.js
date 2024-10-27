36c417af247c66c3dffe397e1d0f8f8737c3f71d
Priscilla Mac-Gatus <macgatuspriscilla@gmail.com>
2024-10-27 20:22:25 +0100

    corrected mistake

--- week-1/errors/1.js
+++ week-1/errors/1.js
@@ -1,6 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
 
-//Uncaught TypeError: Assignment to constant variable.the key word const cannot be reassigned
+//The variable age was reassigned to a expression
 
 let age = 33;
 age = age + 1;
