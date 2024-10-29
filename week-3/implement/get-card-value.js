76cbbe39d6a1c4a5cda859f399a950a480bbdd45
Afere precious <preciousafere@gmail.com>
2024-10-29 20:43:21 +0100

    Implement get-card-value.js solution

--- week-3/implement/get-card-value.js
+++ week-3/implement/get-card-value.js
@@ -29,3 +29,49 @@
 // Given a card with an invalid rank (neither a number nor a recognized face card),
 // When the function is called with such a card,
 // Then it should throw an error indicating "Invalid card rank."
+
+//Solution
+function getCardValue(card) {
+  const cardRank = card.slice(0, card.length - 1); // Extract the rank from the card
+
+  //Handle number cards (2-10)
+  if (cardRank >= "2" && cardRank <= "9") {
+    return Number(cardRank);
+  } else if (cardRank === "10") {
+    return 10;
+    //Handle face card (J, Q, K)
+  } else if (cardRank === "J" || cardRank === "Q" || cardRank === "K") {
+    return 10;
+    // Handle Ace (A):
+  } else if (cardRank === "A") {
+    return 11;
+  }
+
+  //Handle invalid card
+  throw new Error("Invalid card rank.");
+}
+//Assertion for the function
+try {
+  //Handle number cards (2-10)
+  console.log(getCardValue("4♠")); //Expected result :4
+  console.log(getCardValue("6♥")); //Expected result :6
+  console.log(getCardValue("9♣")); //Expected result :9
+  console.log(getCardValue("7♦")); //Expected result :7
+  // Handle card "10"
+  console.log(getCardValue("10♠")); // Expected result: 10
+  //Handle face card (J, Q, K)
+  console.log(getCardValue("J♥")); //Expected result :10
+  console.log(getCardValue("K♣")); //Expected result :10
+  console.log(getCardValue("Q♣")); //Expected result :10
+  // Handle Ace (A):
+  console.log(getCardValue("A♦")); //Expected result :11
+} catch (error) {
+  console.log("Expected error message", error.message);
+}
+try {
+  console.log(getCardValue("Z♠")); //Invalid card
+  console.log(getCardValue("11♠")); //Invalid card
+} catch (error) {
+  console.log("Expected error message", error.message);
+}
+console.log("All test passed");
