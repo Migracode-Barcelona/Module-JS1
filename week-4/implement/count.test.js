405e03cf666d419b399e7689f1130011387e1522
Afere precious <preciousafere@gmail.com>
2024-11-07 15:51:08 +0100

    solution for count.test.js

--- week-4/implement/count.test.js
+++ week-4/implement/count.test.js
@@ -15,3 +15,20 @@
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+//solution
+function countChar(str, char) {
+  let count = 0;
+  //we iterate
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countChar("aaaaa", "a")); //expected output 5
+console.log(countChar("hello world", "l")); //expected output 3
+console.log(countChar("precious", "i")); //expected output 1
+console.log(countChar("hello world", "z")); // expected output 0
+console.log(countChar("hello world", "H")); // expected output 0
