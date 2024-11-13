de2e1978c67a7e94aa78c4ec71f1ac735d828a8a
Priscilla Mac-Gatus <macgatuspriscilla@gmail.com>
2024-11-13 15:14:37 +0100

    Added an assertion to count.test

--- week-4/implement/count.test.js
+++ week-4/implement/count.test.js
@@ -20,3 +20,9 @@ function countChar(input, character) {
   const count = input.split(character).length - 1;
   return count === 0 ? 0 : count;
 }
+
+const expectedExample1 = 2;
+const expectedExample2 = 2;
+
+console.assert(countChar("Zanzibar zoo", "z") === expectedExample1); // Output: 2
+console.assert(countChar("Goose", "o") === expectedExample2); // Output: 2
