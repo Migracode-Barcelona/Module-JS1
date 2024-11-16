0d647ebaa70851a29b9837448c4f11eba4ec65cb
¨Deborah <deborahgamedze7@gmail.com>
2024-11-16 06:29:24 +0100

    i explained why get last is not working .

--- week-1/exercises/random.js
+++ week-1/exercises/random.js
@@ -4,6 +4,9 @@ const maximum = 100;
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
 // In this exercise, you will need to work out what num represents?
+//num represents a random  interger generated within a specified range from minimun to maximum which can be from 1 to 100
+
 // Try breaking down the expression and using documentation to explain what it means
+
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num several times to build an idea of what the program is doing
--- week-2/debug/2.js
+++ week-2/debug/2.js
@@ -12,3 +12,4 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+//The issue lies in the num variable. num is declared as a constant (const num = 103) outside the function. Inside the getLastDigit function, it tries to use this constant num instead of the value passed to the function when it is called (like getLastDigit(42)).
