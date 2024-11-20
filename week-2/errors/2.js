d558e5707f8ec1ed45a21e7493e0366064e4a322
¨Deborah <deborahgamedze7@gmail.com>
2024-11-20 09:40:13 +0100

    i predicted and explained what will happen

--- week-2/errors/2.js
+++ week-2/errors/2.js
@@ -1,5 +1,11 @@
 
 // Predict and explain first...
+//The code will throw a SyntaxError because parameter names in a function declaration cannot be literal values like 3. In JavaScript, function parameters must be variable names, not numbers, strings, or other literals.
+
+
+
+
+
 // this function should square any number but instead we're going to get an error
 // what is happening? How can we fix it?
 
