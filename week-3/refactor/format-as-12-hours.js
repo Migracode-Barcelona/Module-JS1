1b4fdae7641e6d5f04d3df675d7e1dd1e2642b4e
¨Deborah <deborahgamedze7@gmail.com>
2024-11-20 19:56:36 +0100

    second solution

--- week-3/refactor/format-as-12-hours.js
+++ week-3/refactor/format-as-12-hours.js
@@ -4,3 +4,13 @@ function isLeapYear(year) {
   }
   return false;
 }
+function isLeapYear(year) {
+  const divisibleBy4 = year % 4 === 0;
+  const divisibleBy100 = year % 100 === 0;
+  const divisibleBy400 = year % 400 === 0;
+
+  if ((divisibleBy4 && !divisibleBy100) || divisibleBy400) {
+    return true;
+  }
+  return false;
+}
