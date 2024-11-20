85a6be03032e7c8a9fefee2b4dfabaa9b12e38cc
¨Deborah <deborahgamedze7@gmail.com>
2024-11-20 09:32:18 +0100

    i predicted and explained what happens to code

--- week-2/errors/1.js
+++ week-2/errors/1.js
@@ -1,4 +1,8 @@
 // Predict and explain first...
+//The variable decimalNumber is declared within the function scope (using const) and is not accessible outside of it. This creates a scope conflict when trying to access decimalNumber globally.
+
+
+
 // Write down the error you predict will be raised
 // Why will an error occur when this program runs?
 // Play computer with the example to work out what is going on
