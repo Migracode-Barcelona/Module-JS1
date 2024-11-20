cfe1d73b68773556a63150e16803757f4ef3a5fb
¨Deborah <deborahgamedze7@gmail.com>
2024-11-20 09:25:38 +0100

    i explained  and predicted what the function does

--- week-2/errors/0.js
+++ week-2/errors/0.js
@@ -1,4 +1,8 @@
 // Predict and explain first...
+
+//The function takes a string str as input and attempts to capitalize the first character of the string using:
+//Possible error: If str is empty ("") or not a string (e.g., null or undefined), then str[0] will either be undefined or nonexistent. Calling .toUpperCase() on undefined will result in a TypeError.
+
 // write down the error you predict will be raised
 // then call the function capitalise with a string input
 // interpret the error message and figure out why it's happening, if your prediction was wrong
