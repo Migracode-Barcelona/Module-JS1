0e2a89bfe3dc7e3dc31ab2a3b814196e3b317075
¨Deborah <deborahgamedze7@gmail.com>
2024-11-25 13:11:28 +0100

    completed exercise

--- week-4/implement/get-ordinal-number.test.js
+++ week-4/implement/get-ordinal-number.test.js
@@ -1,4 +1,19 @@
-// In this week's prep, we started implementing getOrdinalNumber
-
-// continue testing and implementing getOrdinalNumber for additional cases
-// Write your tests using Jest - remember to run your tests often for continual feedback
+function creditCard(value) {
+  const cardPattern = /^\d{16}$/.test(value);
+  if (!cardPattern) {
+    return "Invalid: Must be 16 digits and all numbers";
+  }
+  const uniqueDigits = new Set(value.split("")).size;
+  if (uniqueDigits < 2) {
+    return "Invalid: Must contain at least two different digits";
+  }
+  if (parseInt(value.slice(-1)) % 2 !== 0) {
+    return "Invalid: Final digit must be even";
+  }
+  const sum = Array.from(value).reduce((a, b) => a + parseInt(b), 0);
+  if (sum <= 16) {
+    return "Invalid: Sum of all digits must be greater than 16";
+  }
+  return "Valid credit card number";
+}
+module.exports = creditCard;
